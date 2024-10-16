b055efeb60fd910d0cc0e6aa495b2be3992eb32c
DanWolberg <dan9074@gmail.com>
2024-10-16 19:16:40 +0300

    Break (step by step) MXG Control Examples From 0 to 8 (full functoinality)

--- Day2/DesignerAndMXG/main_mxg_2.py
+++ Day2/DesignerAndMXG/main_mxg_2.py
@@ -14,19 +14,41 @@ class LabDemoMxgControl(QMainWindow):
         loadUi("BasicMxgControl.ui", self)
 
         self.setWindowTitle("MXG Control")
-        
+
+        # Interface of the GUI Widgets to the Python code
+
         # Object.Event.connect(callback)
         # or alternatively:
         # getattr(self.Object, 'Event').connect(callback)
         # Connect GUI objects to callback functions for events
         # Object = self.pushButton, Event = 'clicked', callback = self.cb_connect
-        self.pushButton .clicked.connect(self.cb_connect   )
+        self.pushButton  .clicked.connect(self.cb_connect   )
+        self.pushButton_2.clicked.connect(self.cb_rf_on_off )
+        self.pushButton_3.clicked.connect(self.cb_mod_on_off)
 
+    # Callback function for the Connect button
+    # That is a checkable button
     def cb_connect(self):
         if self.pushButton.isChecked():
             print("Connect button Checked")
         else:
             print("Connect button Cleared")
+
+    # Callback function for the RF On/Off button
+    # That is a checkable button
+    def cb_rf_on_off(self):
+        if self.pushButton_2.isChecked():
+            print("RF On")
+        else:
+            print("RF Off")
+
+    # Callback function for the Modulation On/Off button
+    # That is a checkable button
+    def cb_mod_on_off(self):
+        if self.pushButton_3.isChecked():
+            print("Modulation On")
+        else:
+            print("Modulation Off")
 
 if __name__ == "__main__":
     # Initializes the application and prepares it to run a Qt event loop
