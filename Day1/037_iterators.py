96c14936e33059049e89638239b4524d09e92d15
Nir Tal <nir@transigo.io>
2024-10-27 12:49:08 +0200

    Split 37 to 37 and 38. Added up to 40

--- Day1/037_iterators.py
+++ Day1/037_iterators.py
@@ -1,3 +1,5 @@
+print("Results")
+print("-------")
 # Sample data
 students    = ["Alice", "Bob", "Charlie", "David", "Eve", "Don"]
 grades      = [85, 92, 78, 95, 88, 99, 77]
@@ -8,7 +10,6 @@ uppercase_names = list(map(str.upper, students))
 print("Uppercase names:", uppercase_names)
 
 uppercase_names = [name.upper() for name in students]
-
 
 # Using zip() to combine students with their grades
 student_grades = list(zip(students, grades))
@@ -28,44 +29,4 @@ for i,x in enumerate(students):
         all_students[x] = {"grade": grades[i], "attendance": attendances[i]}
     except Exception:
         break
-
 print(all_students)
-
-all_students ={}
-for student, grade, attendance in zip(students, grades, attendances):
-    all_students[student] = {"grade": grade, "attendance": attendance}
-print(all_students)
-
-all_students = {}
-all_students = {student: {"grade": grade, "attendance": attendance} for student, grade, attendance in zip(students, grades, attendances)}
-print(all_students)
-
-def lauda_student(x):
-    return x>90
-
-lauda = list(filter(lauda_student, grades))
-print(lauda)
-
-lauda = list(filter(lambda x: x>90, grades))
-print(lauda)
-
-
-
-lauda = [grade for grade in grades if grade > 90]
-print(lauda)
-
-grades_sorted = list(sorted(grades))
-print(grades_sorted)
-
-
-# First, let's zip only students and grades
-student_grades = zip(students, grades)
-
-# Then sort by grade (second element of each tuple) in descending order
-sorted_students = sorted(
-    student_grades,
-    key=lambda x: x[1],  # Sort based on the grade
-)
-
-for student, grade in sorted_students:
-    print(f"{student}: {grade}")
