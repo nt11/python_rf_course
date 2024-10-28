e11d8d57223bfb188a362fb32cb2763aa428de57
Nir Tal <nir@transigo.io>
2024-10-28 16:50:33 +0200

    Fixed 024 regex example. Added a Boolean example 008c

--- Day1/024_regex.py
+++ Day1/024_regex.py
@@ -1,4 +1,5 @@
-import re
+print("Results")
+print("-------")
 
 text = """
 device = ETH0123, ip=192.168.1.122, status=UP
@@ -7,6 +8,6 @@ device=ETH0124, ip=192.168.1.212, status=UP
 router=RT01, ip=192.168.1.191, status=UP
 """
 
-regexp = r'(?:device\s*=\s*|system\s*=\s*|router\s*=\s*)([0-9A-Z]+)'
-matches = re.findall(regexp, text)
-print(matches)
+import re
+regexp = r'(?:device\s*=\s*|system\s*=\s*)([0-9A-Z]+)\s*,'
+print(re.findall(regexp,text))
