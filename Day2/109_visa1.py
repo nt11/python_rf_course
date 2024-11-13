b48df5f9bca4acff6669e83810ebd9bf19054597
DanWolberg <dan9074@gmail.com>
2024-11-13 21:22:53 +0200

    pyvisa basic

--- Day2/109_visa1.py
+++ Day2/109_visa1.py
@@ -1,22 +1,30 @@
 import pyvisa
-import pyvisa-py # Alternatively install NI drivers
 
-rm = pyvisa.ResourceManager()
 
-try:
-    # Connect to instrument
-    instr = rm.open_resource('TCPIP::192.168.1.100::INSTR')
-    instr.timeout = 5000
+if __name__ == '__main__':
+    print("Results")
+    print("-------")
+    rm = pyvisa.ResourceManager('@py')
 
-    # Basic IEEE-488.2 commands
-    idn = instr.query('*IDN?')  # Get ID
-    manufacturer, model, serial, firmware = idn.strip().split(',')
+    try:
+        # Connect to instrument
+        instr = rm.open_resource('TCPIP::10.0.0.7::INSTR')
+        instr.timeout = 5000
 
-    print(f"Manufacturer: {manufacturer}")
-    print(f"Model: {model}")
-    print(f"Serial Number: {serial}")
-    print(f"Firmware Version: {firmware}")
+        # Basic IEEE-488.2 commands
+        idn = instr.query('*IDN?')  # Get ID
+        manufacturer, model, serial, firmware = idn.strip().split(',')
 
-    instr.write('*RST')  # Reset to default
-    instr.write('*CLS')  # Clear status
+        print(f"Manufacturer: {manufacturer}")
+        print(f"Model: {model}")
+        print(f"Serial Number: {serial}")
+        print(f"Firmware Version: {firmware}")
+
+        instr.write('*RST')  # Reset to default
+        instr.write('*CLS')  # Clear status
+    except pyvisa.errors.VisaIOError as e:
+        print(f"Error: {e}")
+    finally:
+        instr.close()
+        rm.close()
 
