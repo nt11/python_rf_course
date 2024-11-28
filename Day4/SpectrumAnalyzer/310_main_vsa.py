4d86af59429bdbb7377d951dd3de945faa6001d3
DanWolberg <dan9074@gmail.com>
2024-11-28 14:26:44 +0200

    define logging widget in utils

--- Day4/SpectrumAnalyzer/310_main_vsa.py
+++ Day4/SpectrumAnalyzer/310_main_vsa.py
@@ -14,8 +14,9 @@ from    time               import sleep
 import numpy as np
 import logging
 
-from python_rf_course.utils.pyqt2python import h_gui
-from python_rf_course.utils.plot_widget import PlotWidget
+from python_rf_course.utils.pyqt2python     import h_gui
+from python_rf_course.utils.plot_widget     import PlotWidget
+from python_rf_course.utils.logging_widget  import setup_logger
 
 from o310_long_process import LongProcess
 
@@ -24,41 +25,6 @@ def is_valid_ip(ip:str) -> bool:
     # Regular expression pattern for matching IP address
     ip_pattern = r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
     return re.match(ip_pattern, ip) is not None
-
-class QTextBrowserHandler(logging.Handler):
-    def __init__(self, text_browser):
-        super().__init__()
-        self.text_browser = text_browser
-        # Use same formatter as console handler
-        self.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-
-    def emit(self, record):
-        msg = self.format(record)
-        self.text_browser.append(msg)
-
-# Dual logger setup (console and QTextBrowser)
-def setup_logger(text_browser=None, name='sa_log' , level=logging.DEBUG, is_console=True):
-    # Root logger
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-
-    # Clear existing handlers to prevent duplicates
-    logger.handlers.clear()
-
-    # Console Handler (keeps console output)
-    if is_console:
-        console_handler = logging.StreamHandler(sys.stdout)
-        console_handler.setLevel(level)
-        console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-        logger.addHandler(console_handler)
-
-    # QTextBrowser Handler (if provided)
-    if text_browser:
-        text_browser_handler = QTextBrowserHandler(text_browser)
-        text_browser_handler.setLevel(level)
-        logger.addHandler(text_browser_handler)
-
-    return logger
 
 
 # Suppress logging of specified modules (only show errors)
