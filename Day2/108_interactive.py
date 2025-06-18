dffbd08e4acbe4f67badcb636e19449f58cd1f04
DanWolberg <dan9074@gmail.com>
2025-06-18 13:09:29 +0300

    interactive mode

--- Day2/108_interactive.py
+++ Day2/108_interactive.py
@@ -1,9 +1,9 @@
+import matplotlib
+matplotlib.use('TkAgg')  # Must be BEFORE importing pyplot
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     # Create data
-    import matplotlib
-    matplotlib.use('TkAgg')
     plt.ion()
 
     x = [1, 2, 3, 4, 5]
